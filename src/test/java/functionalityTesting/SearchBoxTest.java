3073d4597d164ef9c52485547ade9b39ced36495
mariyamarych <live-learn@mail.ru>
2020-02-13 18:04:09 -0600

    2st part

--- src/test/java/functionalityTesting/SearchBoxTest.java
+++ src/test/java/functionalityTesting/SearchBoxTest.java
@@ -2,8 +2,11 @@ package functionalityTesting;
 
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -12,64 +15,10 @@ import pages.SearchBoxPage;
 import utilities.Config;
 import utilities.Driver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchBoxTest {
-
-    SearchBoxPage searchBoxPage = new SearchBoxPage();
-
-
-    @BeforeClass
-    public void setUp() {
-        Driver.getDriver().get("http://app.briteerp.com/web/login");
-    }
-
-    @Test
-    public void LoginPage() {
-        searchBoxPage.usernameInput.sendKeys(Config.getProperties("UsernameManager3"));
-        searchBoxPage.passwordInput.sendKeys(Config.getProperties("passwordManager3"));
-        searchBoxPage.loginButton.click();
-    }
-
-    @Test
-    public void selectInvoiceModule() {
-        searchBoxPage.InvoicingModule.click();
-        Assert.assertEquals(searchBoxPage.HeaderTextCustomerInvoices.getText(), "Customer Invoices");
-    }
-
-    @Test
-    public void searchBoxLocation() {
-        Assert.assertTrue(searchBoxPage.searchBox.isDisplayed());
-    }
-
-    @Test
-    public void searchSignBox() {
-        searchBoxPage.searchSignBox.click();
-    }
-
-    @Test
-    public void verifiyngFiltersDisplayed() {
-        Assert.assertTrue(searchBoxPage.FiltersModule.isDisplayed());
-    }
-
-    @Test
-    public void verifiyngGroupByDisplayed() {
-        Assert.assertTrue(searchBoxPage.GroupByModule.isDisplayed());
-    }
-
-    @Test
-    public void verifiyngFavoritesDisplayed() {
-        Assert.assertTrue(searchBoxPage.FavoritesModule.isDisplayed());
-    }
-
-
-    @AfterClass
-    public void closeUp(){
-        Driver.quitDriver();
-}
-
-
-
-
-
 
 
 
