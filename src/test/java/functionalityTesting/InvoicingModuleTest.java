827466d636b252a885d515b082de9816ab2cb4a8
Meergul <mirgulyar@gmail.com>
2020-02-22 12:40:29 -0600

    Meergul last code

--- src/test/java/functionalityTesting/InvoicingModuleTest.java
+++ src/test/java/functionalityTesting/InvoicingModuleTest.java
@@ -1,4 +1,66 @@
 package functionalityTesting;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.InvoicingModulePage;
+import pages.LoginPage;
+import utilities.Config;
+import utilities.Driver;
+
 public class InvoicingModuleTest {
+
+
+    LoginPage loginPage = new LoginPage();
+    HomePage homePage = new HomePage();
+    InvoicingModulePage invoicingModulePage = new InvoicingModulePage();
+
+
+
+    @BeforeMethod
+    public void login(){
+        Driver.getDriver().get(Config.getProperties("url2"));
+        loginPage.usernameInput.sendKeys(Config.getProperties("Username4"));
+        loginPage.passwordInput.sendKeys(Config.getProperties("passwordUser4"));
+        loginPage.loginButton.click();
+        homePage.invoicingButton.click();
+
+    }
+
+
+@Test
+
+    public void CustomerInvoicesPages(){
+
+
+       Assert.assertTrue(invoicingModulePage.CreateInvoicesTabButton.isDisplayed(),"Customer Invoices Tab Button is not Displayed");
+       Assert.assertTrue(invoicingModulePage.CreateInvoicesListButton.isDisplayed(),"Customer Invoices List Button is not Displayed");
+       Assert.assertTrue(invoicingModulePage.CreateInvoicesCalendarButton.isDisplayed(),"Customer Invoices Calendar Button is not Displayed");
+       Assert.assertTrue(invoicingModulePage.CreateInvoicesGraphButton.isDisplayed(),"Customer Invoices Graph Button is not Displayed");
+       Assert.assertTrue(invoicingModulePage.CreateInvoicesKanbanButton.isDisplayed(),"Customer Invoices Kanban Button is not Displayed");
+       Assert.assertTrue(invoicingModulePage.CreateInvoicesPivotButton.isDisplayed(),"Customer Invoices Pivot Button is not Displayed");
+       Assert.assertTrue(invoicingModulePage.CreateInvoicesSearchButton.isDisplayed(),"Customer Invoices Search Button is not Displayed");
+       Assert.assertTrue(invoicingModulePage.CreateInvoicesTabButton.isDisplayed(),"Customer Invoices Tab Button is not Displayed");
+       Assert.assertTrue(invoicingModulePage.CreateInvoicesPreviousArrow.isDisplayed(),"Customer Invoices Previous Arrow is not Displayed");
+       Assert.assertTrue(invoicingModulePage.CreateInvoicesNextArrow.isDisplayed(),"Customer Invoices Next Arrow is not Displayed");
+       Assert.assertTrue(invoicingModulePage.CreateInvoicesTab.isDisplayed(),"Customer Invoices Create Invoice is not Displayed");
+       Assert.assertTrue(invoicingModulePage.CustomerCreditNotes.isDisplayed(),"Customer Credit Notes is not Displayed");
+       Assert.assertTrue(invoicingModulePage.FirstPayments.isDisplayed(),"Customer First Payment Tab is not Displayed");
+       Assert.assertTrue(invoicingModulePage.LoginAvatarImage.isDisplayed(),"Customer Login Avatar Image is not Displayed");
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
 }
--- src/test/java/pages/InvoicingModulePage.java
+++ src/test/java/pages/InvoicingModulePage.java
@@ -13,12 +13,54 @@ public class InvoicingModulePage {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
-    @FindBy(xpath = "//tbody[@class='ui-sortable']/tr/td[8]")
-    public List<WebElement> tableTotal;
+  //  @FindBy(xpath = "//tbody[@class='ui-sortable']/tr/td[8]")
+   // public List<WebElement> tableTotal;
 
-    @FindBy(xpath = "//tbody[@class='ui-sortable']/tr/td[10]")
-    public List<WebElement> tableStatus;
+   // @FindBy(xpath = "//tbody[@class='ui-sortable']/tr/td[10]")
+   // public List<WebElement> tableStatus;
 
+    @FindBy(xpath = "//button[@class='btn btn-primary btn-sm o_list_button_add']")
+    public WebElement CreateInvoicesTabButton;
+
+    @FindBy(xpath = "//button[@class='btn btn-sm btn-default o_button_import']")
+    public WebElement CreateInvoicesImportButton;
+
+    @FindBy(xpath = "//input[@class='o_searchview_input']")
+    public WebElement CreateInvoicesSearchButton;
+
+    @FindBy(xpath = "//button[@class='btn btn-icon fa fa-lg fa-list-ul o_cp_switch_list active']")
+    public WebElement CreateInvoicesListButton;
+
+    @FindBy(xpath = "//button[@aria-label='kanban']")
+    public WebElement CreateInvoicesKanbanButton;
+
+    @FindBy(xpath = "//button[@class='btn btn-icon fa fa-lg fa-calendar o_cp_switch_calendar']")
+    public WebElement CreateInvoicesCalendarButton;
+
+    @FindBy(xpath = "//button[@class='btn btn-icon fa fa-lg fa-bar-chart o_cp_switch_graph']")
+    public WebElement CreateInvoicesGraphButton;
+
+    @FindBy(xpath = "//button[@aria-label='pivot']")
+    public WebElement CreateInvoicesPivotButton;
+
+
+    @FindBy(xpath = "//button[@aria-label='Previous']")
+    public WebElement CreateInvoicesPreviousArrow;
+
+    @FindBy(xpath = "//button[@aria-label='Next']")
+    public WebElement CreateInvoicesNextArrow;
+
+    @FindBy(xpath = "//span[contains(text(),'Customer Invoices')]")
+    public WebElement CreateInvoicesTab;
+
+    @FindBy(xpath = "//span[contains( text(),'Customer Credit Notes')]")
+    public WebElement CustomerCreditNotes;
+
+    @FindBy(xpath = "//a[@data-action-id='246']")
+    public WebElement FirstPayments;
+
+    @FindBy(xpath = "//img[@class='img-circle oe_topbar_avatar']")
+    public WebElement LoginAvatarImage;
 
 
 }
